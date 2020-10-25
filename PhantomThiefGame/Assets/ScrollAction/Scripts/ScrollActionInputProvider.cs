fb34952a7569907fff2cbb00e85afa17df77470e
DESKTOP-SRM98JO\ylink <ylink.90@outlook.jp>
2020-10-25 19:00:56 +0900

    Player Input触りました

---
+++ PhantomThiefGame/Assets/ScrollAction/Scripts/ScrollActionInputProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScrollActionInputProvider : MonoBehaviour
+{
+    public float moveHorizontal;
+    public bool isJumpButtonDown;
+
+    private PlayerInput playerInput;
+    private InputActionMap actionMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        actionMap = playerInput.currentActionMap;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveHorizontal = actionMap["Move"].ReadValue<float>();
+        isJumpButtonDown = actionMap["Jump"].triggered;
+        //Debug.Log(isJumpButtonDown);
+    }
+}
