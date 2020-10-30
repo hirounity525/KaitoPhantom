9ce00f8c3cb379c4284d2a5a34bf777a44b17325
DESKTOP-C680O1G\81906 <2010291improve@gmail.com>
2020-10-31 00:55:28 +0900

    敵の出現

--- PhantomThiefGame/Assets/Defense/Scripts/Enemy/DefenseEnemyMover.cs
+++ PhantomThiefGame/Assets/Defense/Scripts/Enemy/DefenseEnemyMover.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DefenseEnemyControler : MonoBehaviour
+public class DefenseEnemyMover : MonoBehaviour
 {
     [SerializeField] private float enemySpeed;
     private Rigidbody rb;
     private Transform enemyTrans;
+    [SerializeField] private Transform[] moveTrans;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class DefenseEnemyControler : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
         rb.velocity = enemyTrans.right * enemySpeed;
     }
--- PhantomThiefGame/Assets/Defense/Scripts/Player/DefensePlayerAttacker.cs
+++ PhantomThiefGame/Assets/Defense/Scripts/Player/DefensePlayerAttacker.cs
@@ -7,7 +7,8 @@ public class DefensePlayerAttacker : MonoBehaviour
     [SerializeField] private ObjectPool bulletPool;
     [SerializeField] private DefenseInputProvider inputProvider;
     [SerializeField]private Transform gunNozzlePosTrans;
-
+    [SerializeField] private float shootWaitTime;
+    private bool canShoot=true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +18,22 @@ public class DefensePlayerAttacker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(canShoot)
         if (inputProvider.isAttackButtonDown) 
         {
             GameObject bullet = bulletPool.GetObject();
             bullet.transform.position = gunNozzlePosTrans.position;
             bullet.transform.rotation = gunNozzlePosTrans.rotation;
             bullet.GetComponent<DefenseBulletController>().MoveBullet();
-        }
+                StartCoroutine(StartMove());
+            }
+    }
+    private IEnumerator StartMove()
+    {
+        canShoot = false;
+
+        yield return new WaitForSeconds(shootWaitTime);
+
+        canShoot = true;
     }
 }
