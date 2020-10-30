97060ac9d254a0499e67927cc3be8a9a149a358e
DESKTOP-C680O1G\81906 <2010291improve@gmail.com>
2020-10-30 18:24:40 +0900

    敵の追加、敵の移動を追加

---
+++ PhantomThiefGame/Assets/Defense/Scripts/DefenseEnemyControler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenseEnemyControler : MonoBehaviour
+{
+    [SerializeField] private float enemySpeed;
+    private Rigidbody rb;
+    private Transform enemyTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        enemyTrans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = enemyTrans.right * enemySpeed;
+    }
+}
