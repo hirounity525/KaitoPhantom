107fca21cda9e4c1803953e89775574098fb075c
DESKTOP-C680O1G\81906 <2010291improve@gmail.com>
2020-11-15 14:38:46 +0900

    敵の移動、弾の移動など（ミスって全て消えました）

--- PhantomThiefGame/Assets/Shooting/Scripts/ShootingEnemyMover.cs
+++ PhantomThiefGame/Assets/Shooting/Scripts/ShootingEnemyMover.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ShootingEnemyMover : MonoBehaviour
 {
+   [SerializeField] private float enemySpeed;
+
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(-1 * enemySpeed, 0, 0);
     }
 }
