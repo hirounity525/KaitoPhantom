5d3747aee5d4eece8377287e271afeec5b245f5c
DESKTOP-C680O1G\81906 <2010291improve@gmail.com>
2020-12-15 21:08:27 +0900

    Runゲームのシステム

---
+++ PhantomThiefGame/Assets/Run&Jump/Scripts/Player/RunPlayerHPController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunPlayerHPController : MonoBehaviour
+{
+    [SerializeField] private int playerFirstHP;
+    [SerializeField] private float unavailableColliderTime;
+
+    private Transform playerTrans;
+
+    [SerializeField] private int playerNowHP;
+
+    private bool unavailable = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTrans = GetComponent<Transform>();
+
+        playerNowHP = playerFirstHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerNowHP <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (playerTrans.position.y <= -8)
+        {
+            playerNowHP = 0;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject Obstacle = other.gameObject;
+
+        if (!unavailable)
+        {
+            if (Obstacle.tag == "Enemy")
+            {
+                playerNowHP--;
+                StartCoroutine(UnavailableCollider());
+            }
+        }
+
+    }
+
+    private IEnumerator UnavailableCollider()
+    {
+        unavailable = true;
+
+        yield return new WaitForSeconds(unavailableColliderTime);
+
+        unavailable = false;
+    }
+}
--- PhantomThiefGame/Assets/Run&Jump/Scripts/Player/RunPlayerMover.cs
+++ PhantomThiefGame/Assets/Run&Jump/Scripts/Player/RunPlayerMover.cs
@@ -6,15 +6,19 @@ public class RunPlayerMover : MonoBehaviour
 {
     [SerializeField] private float jumpPower;
     [SerializeField] private RunInputProvider inputProvider;
+    [SerializeField] private float rotationAngle;
 
     private Rigidbody rb;
+    private Transform playerTrans;
 
     private bool isJump=true;
+    private bool canSliding=true;
     [SerializeField] private int jumpCount;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        playerTrans = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -25,8 +29,10 @@ public class RunPlayerMover : MonoBehaviour
         {
             if (inputProvider.isJumpButtunDown)
             {
+                rb.velocity = new Vector3(0, 0, 0);
                 rb.AddForce(new Vector3(0, jumpPower, 0), ForceMode.Impulse);
                 jumpCount++;
+                canSliding = false;
 
                 if (jumpCount >= 2)
                 {
@@ -36,6 +42,14 @@ public class RunPlayerMover : MonoBehaviour
             }
         }
 
+        if (inputProvider.isSlidingButtunDown > 0 && canSliding)
+        {
+            playerTrans.rotation =Quaternion.Euler(0,0,rotationAngle);
+        }
+        else
+        {
+            playerTrans.rotation = new Quaternion(0, 0, 0, 0);
+        }
 
     }
 
@@ -44,6 +58,7 @@ public class RunPlayerMover : MonoBehaviour
         GameObject groundObj = collision.gameObject;
         if (groundObj.tag == "Ground")
         {
+            canSliding = true;
             isJump = true;
             jumpCount = 0;
         }
