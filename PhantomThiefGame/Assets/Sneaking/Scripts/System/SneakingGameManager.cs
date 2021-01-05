49af8b2d09abd979ccfa1fcec4a41c92d62c3bc7
hiro <hiroritsumei@gmail.com>
2021-01-05 14:00:16 +0900

    インプット、修正

--- PhantomThiefGame/Assets/Sneaking/Scripts/System/SneakingGameManager.cs
+++ PhantomThiefGame/Assets/Sneaking/Scripts/System/SneakingGameManager.cs
@@ -6,6 +6,10 @@ public class SneakingGameManager : MonoBehaviour
 {
     [SerializeField] private GameState gameState;
 
+    [SerializeField] private TimelineController startTimeline;
+
+    private bool isFirstStatePlay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +22,28 @@ public class SneakingGameManager : MonoBehaviour
         switch (gameState)
         {
             case GameState.START:
+
+                //スタート演出
+                //タイムラインの再生
+                if (!isFirstStatePlay)
+                {
+                    startTimeline.Play();
+                    isFirstStatePlay = true;
+                }
+
+                //再生後に、メインに移動
+                if (startTimeline.isFinish)
+                {
+                    gameState = GameState.MAIN;
+                    isFirstStatePlay = false;
+                }
+
                 break;
             case GameState.MAIN:
+                //インプット使えるようにする
+                //ポーズボタンを押したら、ポーズに移動する
+                //プレイヤーが見つかったら、ゲームオーバーに移動する
+                //プレイヤーがゴールしたら、クリアに移動する
                 break;
             case GameState.PAUSE:
                 break;
