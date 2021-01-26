b4850bbb14100f58b67598c9aec8f767153ab200
DESKTOP-C680O1G\81906 <2010291improve@gmail.com>
2021-01-26 17:18:15 +0900

    runゲームのゲームデザイン

--- PhantomThiefGame/Assets/Run&Jump/Scripts/Player/RunPlayerMover.cs
+++ PhantomThiefGame/Assets/Run&Jump/Scripts/Player/RunPlayerMover.cs
@@ -7,13 +7,15 @@ public class RunPlayerMover : MonoBehaviour
     [SerializeField] private float jumpPower;
     [SerializeField] private RunInputProvider inputProvider;
     [SerializeField] private float rotationAngle;
+    [SerializeField] private float slidingTime;
 
     private Rigidbody rb;
     private Transform playerTrans;
 
-    private bool isJump=true;
-    private bool canSliding=true;
-    [SerializeField] private int jumpCount;
+    private bool isJump = true;
+    private bool isSliding = false;
+    private bool canSliding = true;
+    private int jumpCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,13 +44,9 @@ public class RunPlayerMover : MonoBehaviour
             }
         }
 
-        if (inputProvider.isSlidingButtunDown > 0 && canSliding)
+        if ((inputProvider.isSlidingButtunDown > 0 && canSliding) || isSliding )
         {
-            playerTrans.rotation =Quaternion.Euler(0,0,rotationAngle);
-        }
-        else
-        {
-            playerTrans.rotation = new Quaternion(0, 0, 0, 0);
+            StartCoroutine(StartSliding());
         }
 
     }
@@ -63,4 +61,20 @@ public class RunPlayerMover : MonoBehaviour
             jumpCount = 0;
         }
     }
+
+    private IEnumerator StartSliding()
+    {
+        playerTrans.rotation = Quaternion.Euler(0, 0, rotationAngle);
+
+        isJump = false;
+        isSliding = true;
+
+        yield return new WaitForSeconds(slidingTime);
+
+        isJump = true;
+        isSliding = false;
+
+        playerTrans.rotation = new Quaternion(0, 0, 0, 0);
+    }
+
 }
